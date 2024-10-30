7a850c8cb1161f8377b850913b37ffe3ccad24d8
Max-499 <max.keune@headflat.com>
2024-10-30 20:36:32 +0100

    Unity Project

---
+++ UnityTransitionMax/Assets/Scripts/FadeMixerGroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public static class FadeMixerGroup
+{
+
+    public static IEnumerator StartFade(AudioMixer audioMixer, string exposedParam, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float currentVol;
+        if(duration < 0.001f)
+        {
+            duration = 0.001f;
+        }
+        audioMixer.GetFloat(exposedParam, out currentVol);
+        currentVol = Mathf.Pow(10, currentVol / 20);
+        float targetValue = Mathf.Clamp(targetVolume, 0.001f, 1);
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
+            audioMixer.SetFloat(exposedParam, Mathf.Log10(newVol) * 20);
+            yield return null;
+        }
+        yield break;
+    }
+}
---
+++ UnityTransitionMax/README.md
@@ -0,0 +1,8 @@
+# Unity-Music-Transitions
+Unity template which allows composers to test bridge transitions between two themes
+
+Video Tutorial:
+https://www.youtube.com/watch?v=on8D3vMwLdE
+
+Optimized for Unity Version:
+2020.1.3f1
